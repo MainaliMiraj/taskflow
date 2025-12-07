c71eaa4e4a1c36f521e82f50062c99287896cdd0
Miraj Mainali <mirajmainali@Mirajs-MacBook-Pro.local>
2025-12-07 17:08:49 +1100

    fixed the nextJS suspense issue

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,14 @@
+import { Suspense } from "react";
 import Dashboard from "./_ui/Dashboard";
 
 export default function Page() {
-  return <Dashboard />;
+  return (
+    <Suspense
+      fallback={
+        <div className="text-primary-700 text-center">Loading dashboard...</div>
+      }
+    >
+      <Dashboard />
+    </Suspense>
+  );
 }
