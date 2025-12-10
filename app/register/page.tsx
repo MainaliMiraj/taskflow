35690ffeec0894f8b6aa839895924ba3a24e9fa2
Miraj Mainali <mirajmainali@Mirajs-MacBook-Pro.local>
2025-12-10 14:03:20 +1100

    revamp:removed image from register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,43 +2,36 @@
 
 import AuthForm from "@/components/authComponent/AuthForm";
 import {useRegister} from "@/hooks/useRegister";
+import NavBar from "@/components/navbar/Navbar";
 
 export default function RegisterPage() {
+    const {handleRegister} = useRegister();
 
-const { handleRegister } = useRegister();
+    return (
+        <>
+            <NavBar/>
+            <div className="flex items-center justify-center mt-12 ">
+                <div className="p-8 w-1/3">
+                    <h2 className="text-3xl font-bold text-center mb-6 bg-gradient-to-r from-primary-600 to-primary-400 bg-clip-text text-transparent">
+                        Welcome to Taskflow
+                    </h2>
 
-  return (
-    <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full margin-none ">
-        <div className="flex items-center justify-center bg-gray-100">
-          <div className="p-8 w-3/4">
-            <h2 className="text-3xl font-bold text-center mb-6 bg-gradient-to-r from-primary-600 to-primary-400 bg-clip-text text-transparent">
-              Welcome to Taskflow
-            </h2>
+                    <AuthForm
+                        showNameField={true}
+                        submitLabel="Sign Up"
+                        onSubmit={handleRegister}/>
 
-            <AuthForm
-              showNameField={true}
-              submitLabel="Sign Up"
-              onSubmit={handleRegister}
-            />
-
-            <p className="text-center text-sm mt-6 text-gray-700">
-              Already have an account?{" "}
-              <a
-                href="/login"
-                className="text-primary-600 hover:underline font-medium"
-              >
-                Login
-              </a>
-            </p>
-          </div>
-        </div>
-
-        <div
-          className="hidden md:block h-screen bg-cover bg-center"
-          style={{ backgroundImage: "url('/login-img.webp')" }}
-        ></div>
-      </div>
-    </div>
-  );
+                    <p className="text-center text-sm mt-6 text-gray-700">
+                        Already have an account?{" "}
+                        <a
+                            href="/login"
+                            className="text-primary-600 hover:underline font-medium"
+                        >
+                            Login
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </>
+    );
 }
